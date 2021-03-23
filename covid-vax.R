688ad46ab65b2bd9f20881a3e9648403d399e93d
jaime <ashander@rff.org>
2021-03-23 13:47:05 -0400

    Fix

--- covid-vax.R
+++ covid-vax.R
@@ -19,14 +19,15 @@ Pfizer Vaccine Data -https://data.cdc.gov/Vaccinations/COVID-19-Vaccine-Initial-
 Janssen Vaccine Data -https://data.cdc.gov/Vaccinations/COVID-19-Vaccine-Distribution-Allocations-by-Juris/w9zu-fywh
 "
 
-juris <- "jurisdiction=District%20of%20Columbia"
+juris <- "?jurisdiction=District%20of%20Columbia"
 
-eps <- list(
+eps0 <- list(
   pfizer = "https://data.cdc.gov/resource/saz5-9hgg.json",
   moderna = "https://data.cdc.gov/resource/b7pe-5nws.json",
   jj = "https://data.cdc.gov/resource/w9zu-fywh.json"
 ) 
-eps <- paste0(eps, juris)
+eps <- paste0(eps0, juris)
+names(eps) <- names(eps0)
 
 get <- function(txt) {
   ddp <- fromJSON(txt) %>%
@@ -55,13 +56,15 @@ lapply(ds[1:2], mungep) -> ds_
 ds[1:2] <- ds_
 ds[3] <- ds__
 
-ds <- lapply(1:3, function(i) {ds[[i]] %>% mutate(type=names(ds)[i]) })
+ds <- lapply(1:3, function(i) {
+  ds[[i]] %>% 
+    mutate(type=names(ds)[i]) })
 d <- do.call(rbind, ds)
 
 
 
 # rule: 6-day lag from allocation to delivery week starting monday
-dw <- d %>% mutate(offset=ifelse(name=='second' &type=='pfizer', 14+6,
+dw <- d %>% mutate(offset=ifelse(name=='second' & type=='pfizer', 14+6,
                                   ifelse(name =='second'&type=='moderna', 6+21, 6))) %>%
   mutate(offset=lubridate::days(offset)) %>%
   mutate( delivery_week=week_of_allocations + offset) %>%
@@ -75,8 +78,11 @@ dw %>% filter(delivery_week - update <=0) ->tmp
 nearest_week <- tmp[which.max(tmp$delivery_week - update), "delivery_week"][[1]]
 this_week <- tmp %>% filter(delivery_week == nearest_week)
 
+write_csv(dw, "all_allocations.csv")
+write_csv(dw %>% group_by(delivery_week) %>% summarize(weekly_total=sum(value)), "weekly_allocations.csv")
 write_csv(this_week, "this_week.csv")
-write_csv(dw, "all_weeks.csv")
+
+
 
 ggplot(dw) + 
   geom_rect(ymin=0, ymax=Inf, xmin=nearest_week, xmax=today(), fill='lightblue', alpha=0.1) +
@@ -86,4 +92,5 @@ ggplot(dw) +
        caption="source CDC: https://dev.socrata.com/foundry/data.cdc.gov/b7pe-5nws")
   #eom_col(aes(delivery_week, value, fill=type))
 
-ggsave("trajectory.png")
+ggsave("trajectory.png", width = 4, height=3)
+ 
